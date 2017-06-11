7efa3236720a985789e7c4d1915e48f5e5921fed
Gustavo Puma <vash47@gmail.com>
2017-06-11 21:53:00 +0100

    spring mvc agregado correctamente al proyecto base

---
+++ proy-base-gaemvc-gradle/src/main/java/pw2/controllers/HolaController.java
@@ -0,0 +1,18 @@
+package pw2.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HolaController{
+	@RequestMapping("/hola")
+	public String mostrarHola(){
+		//usa el ViewResolver
+		return "hola";
+	}
+	@RequestMapping("/")
+	public String mostrarInicio(){
+		//usa el ViewResolver
+		return "inicio";
+	}
+}
